12b1305a4f4c40a219e637defcfd7fb355772f38
himanshututeja1998 <31662677+himanshututeja1998@users.noreply.github.com>
2019-06-24 21:19:33 +0530

    Add files via upload

---
+++ Textual Entailment/hubconf.py
@@ -0,0 +1,19 @@
+dependencies = ['torch', 'tqdm', 'boto3', 'requests', 'regex']
+
+from hubconfs.bert_hubconf import (
+    bertTokenizer,
+    bertModel,
+    bertForNextSentencePrediction,
+    bertForPreTraining,
+    bertForMaskedLM,
+    bertForSequenceClassification,
+    bertForMultipleChoice,
+    bertForQuestionAnswering,
+    bertForTokenClassification
+)
+from hubconfs.gpt_hubconf import (
+    openAIGPTTokenizer,
+    openAIGPTModel,
+    openAIGPTLMHeadModel,
+    openAIGPTDoubleHeadsModel
+)
